5f4a0e2abefd544c3247e7d6765faadc125c7b9a
Fabrice Reiser <reiser.fabrice@gmail.com>
2022-10-24 20:38:31 +0200

    Finalized permutation function

--- Permutation.ps1
+++ Permutation.ps1
@@ -1,36 +1,73 @@
+<#
+    .SYNOPSIS
+    Generates all permutations of n objects with the heap algorithm.
+
+    .DESCRIPTION
+    Generates all permutations of n objects with the heap algorithm.
+
+    .PARAMETER ToPermute
+    Specifies the array with values to permute. 
+    For example (string): 'A','B','C'
+
+    .OUTPUTS
+    All permutations of the input array.
+
+    .EXAMPLE
+    PS> Permutation.ps1 -ToPermute 1,2,3
+    1,2,3
+    2,1,3
+    3,1,2
+    1,3,2
+    2,3,1
+    3,2,1
+
+    .LINK
+    https://github.com/fabrisodotps1/ps-heapalgo-permutations
+#>
 [CmdletBinding()]
-param()
+param(
+    [Parameter(Mandatory=$false)]
+    [Array]$ToPermute = @('A','B','C')
+)
+
+##############################################################
+### Variables                                              ###
+##############################################################
+
+##############################################################
+### Functions                                              ###
+##############################################################
 
 function Add-Permutation {
+    [CmdletBinding()]
     param(
         [Array]$ToPermute,
         [int]$ArrLength
     )
-    $GeneratedPermutations = @()
 
     if ($ArrLength -eq 1) {
-        $GeneratedPermutations += $ToPermute
-        return $GeneratedPermutations
+        # Print permutation
+        $ToPermute -join ','
     } else {
-        $ArrLength = $ArrLength - 1
-        Add-Permutation -ToPermute $ToPermute -ArrLength $ArrLength
-        $i = 0
-        while ($i -lt $ArrLength) {
+        for ($i = 0; $i -lt $ArrLength; $i++) {
+            Add-Permutation -ToPermute $ToPermute -ArrLength ($ArrLength - 1)
+            # If size is even, swap i-th and (ArrLength - 1)-th element
             if ($ArrLength % 2 -eq 0) {
                 $Temp = $ToPermute[$i]
-                $ToPermute[$i] = $ToPermute[$ArrLength]
-                $ToPermute[$ArrLength] = $Temp
+                $ToPermute[$i] = $ToPermute[($ArrLength - 1)]
+                $ToPermute[($ArrLength - 1)] = $Temp
+            # If size is odd, swap 0-th and (ArrLength - 1)-th element
             } else {
                 $Temp = $ToPermute[0]
-                $ToPermute[0] = $ToPermute[$ArrLength]
-                $ToPermute[$ArrLength] = $Temp
+                $ToPermute[0] = $ToPermute[($ArrLength - 1)]
+                $ToPermute[($ArrLength - 1)] = $Temp
             }
-            Add-Permutation -ToPermute $ToPermute -ArrLength $ArrLength
-            $i++
         }
     }
 }
 
-$ToPermute = "A","B"
-$perm = Add-Permutation -ToPermute $ToPermute -ArrLength $ToPermute.Length
-$perm
+##############################################################
+### Main                                                   ###
+##############################################################
+
+Add-Permutation -ToPermute $ToPermute -ArrLength $ToPermute.Length
